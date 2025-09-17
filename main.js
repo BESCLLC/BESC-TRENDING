eaa3a03fada128112e4a4c073629ec9c3c459078
BESC LLC <support@besc.services>
2025-09-17 00:14:58 -0400

    Update main.js

--- main.js
+++ main.js
@@ -5,7 +5,7 @@ import axios from 'axios';
 const {
   TELEGRAM_TOKEN,
   TELEGRAM_CHAT_ID,
-  POLL_INTERVAL_MINUTES = '15',
+  POLL_INTERVAL_MINUTES = '5',
   TRENDING_SIZE = '5',
   NEW_POOL_ALERT_LIQ = '2500',  // min liquidity to trigger alert
   NEW_POOL_ALERT_VOL = '1000'   // min volume to trigger alert
